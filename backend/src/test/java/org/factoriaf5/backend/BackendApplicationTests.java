aaa007f0e2a33c43011724741d677123ee345806
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-01-22 15:38:14 +0100

    comenzados los test aun sin finalizar

--- backend/src/test/java/org/factoriaf5/backend/BackendApplicationTests.java
+++ backend/src/test/java/org/factoriaf5/backend/BackendApplicationTests.java
@@ -1,13 +1,42 @@
 package org.factoriaf5.backend;
 
+import org.factoriaf5.backend.persistence.MovieRepository;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.equalTo;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class BackendApplicationTests {
 
+	@Autowired
+	private MockMvc api;
+
+	@Autowired
+	private MovieRepository repository;
+
+	@BeforeEach
+	void setup() {
+		repository.deleteAll();
+	}
+
 	@Test
-	void contextLoads() {
+	void returnsListOfMovies() throws Exception{
+
+		api.perform(get("/movies"))
+		.andExpectAll(
+			status().isOk(),
+			jsonPath("$[0].title", equalTo("Socorro")));
 	}
 
 }
