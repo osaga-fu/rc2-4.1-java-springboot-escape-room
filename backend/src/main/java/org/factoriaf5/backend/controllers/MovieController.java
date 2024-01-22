c3d885ebb79f5cd5769cf42470fa2410ab6f4fbc
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-01-22 10:57:32 +0100

    añadidos metodos getMovies y createMovie

---
+++ backend/src/main/java/org/factoriaf5/backend/controllers/MovieController.java
@@ -0,0 +1,26 @@
+package org.factoriaf5.backend.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+public class MovieController {
+    
+    @GetMapping("/hello")
+    public String hello() {
+
+        return "Hola mundo!";
+    }
+
+    @GetMapping("/movies")
+    public List<String> getMovies(){
+        return List.of("Película 1", "Película 2", "Película 3");
+    }
+
+    @PostMapping("/movies")
+    public void createMovie(){
+        System.out.println("He sido llamado, soy createMovie");
+    }
+}
